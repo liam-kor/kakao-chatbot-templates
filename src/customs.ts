547bdce39e4d921ce9d77c31494b1c0691f423be
jhkang <winzlove@naver.com>
2021-01-13 18:15:41 +0900

    modified bug : createCarouselWithData

--- src/customs.ts
+++ src/customs.ts
@@ -1,14 +1,29 @@
 import { BasicCard, Carousel, CarouselHeader, CommerceCard } from './cards';
+import * as _ from 'lodash';
 
 export const createCarouselWithData = (
-  card: CommerceCard | BasicCard,
+  cardFormat: CommerceCard | BasicCard,
   dataList: Record<string, any>[],
   header?: CarouselHeader,
 ) => {
   const items = [];
   for (const data of dataList) {
+    const card = _.cloneDeep(cardFormat);
     card.insertData(data);
     items.push(card);
   }
   return new Carousel(items, header);
+};
+
+export const createCardWithData = (
+  card: CommerceCard | BasicCard,
+  data: Record<string, any> | Record<string, any>[],
+  header?: CarouselHeader,
+): Carousel | BasicCard | CommerceCard => {
+  if (Array.isArray(data)) {
+    return createCarouselWithData(card, data, header);
+  } else {
+    card.insertData(data);
+    return card;
+  }
 };
--- tests/components.test.ts
+++ tests/components.test.ts
@@ -1,9 +1,41 @@
+import { createCardWithData } from '../src';
 import { BasicCard } from '../src/cards';
 import { Thumbnail } from '../src/common';
 import { Button } from '../src/directions';
 import { SimpleImage, SimpleText } from '../src/simple';
 
 describe('Component test', () => {
+  const testData = {
+    imageUrl: 'https://naver.com',
+    title: 'title sample',
+    description: 'description sample',
+    price: 10000,
+    order_id: 1234,
+  };
+  const testDataList = [
+    {
+      title: 'title sample 1',
+      description: 'description sample 1',
+      imageUrl: 'https://naver.com/1',
+      price: 10000,
+      order_id: 1111,
+    },
+    {
+      title: 'title sample 2',
+      description: 'description sample 2',
+      imageUrl: 'https://naver.com/2',
+      price: 20000,
+      order_id: 2222,
+    },
+  ];
+  const imageUrlSample = 'https://naver.com/test.jpg';
+  const dynamicImageUrlSample = '{imageUrl}';
+  const thumbnailSample = new Thumbnail({
+    imageUrl: imageUrlSample,
+  });
+  const dynamicThumbnailSample = new Thumbnail({
+    imageUrl: dynamicImageUrlSample,
+  });
   it('Simple Text', () => {
     const textSample = 'simple text';
     const simpleText = new SimpleText(textSample);
@@ -15,25 +47,21 @@ describe('Component test', () => {
     });
   });
   it('Simple Image', () => {
-    const imageSample = 'https://www.naver.com/test.jpg';
     const altTextSample = 'Sample Image';
-    const simpleImage = new SimpleImage(imageSample, altTextSample);
+    const simpleImage = new SimpleImage(imageUrlSample, altTextSample);
     expect(simpleImage).toBeDefined();
     expect(simpleImage.render()).toStrictEqual({
       simpleImage: {
-        imageUrl: imageSample,
+        imageUrl: imageUrlSample,
         altText: altTextSample,
       },
     });
   });
   it('Basic card', () => {
-    const imageUrlSample = 'https://naver.com/test.jpg';
     const basicCardSample = new BasicCard({
       title: 'title',
       description: 'description',
-      thumbnail: new Thumbnail({
-        imageUrl: imageUrlSample,
-      }),
+      thumbnail: thumbnailSample,
       buttons: [
         new Button({
           label: 'label',
@@ -48,4 +76,32 @@ describe('Component test', () => {
   it.todo('Carousel created by BasicCard');
   it.todo('Carousel created by CommerceCard');
   it.todo('Cards with buttons');
+  it('Create BasicCard with Data', () => {
+    const basicCardFormat = new BasicCard({
+      title: '{title}',
+      description: '{description}',
+      thumbnail: dynamicThumbnailSample,
+    });
+    console.log(
+      JSON.stringify(
+        createCardWithData(basicCardFormat, testData).render(),
+        null,
+        2,
+      ),
+    );
+  });
+  it('Create Carousel with Data', () => {
+    const basicCardFormat = new BasicCard({
+      title: '{title}',
+      description: '{description}',
+      thumbnail: dynamicThumbnailSample,
+    });
+    console.log(
+      JSON.stringify(
+        createCardWithData(basicCardFormat, testDataList).render(),
+        null,
+        2,
+      ),
+    );
+  });
 });
